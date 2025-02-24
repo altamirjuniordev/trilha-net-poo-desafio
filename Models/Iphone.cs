35ce9a813056ae0a712fcf1153b30b471ad62a65
altamirjuniordev <altamirjuniordev@hotmail.com>
2025-02-24 14:58:32 -0300

    Commit final do Desafio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone 
     {
+        public Iphone(string numero, string imei, string modelo, int memoria) : base(numero, imei, modelo, memoria)
+        {}
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone.");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "112233", imei: "9876543210", modelo: "1100", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("GitHub");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "654321", imei: "1234567890", modelo: "15", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("DIO");
